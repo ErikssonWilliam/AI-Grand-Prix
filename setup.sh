0af7d6980074b9ba028848725c21b7018fb902c7
William Eriksson <william02erik@gmail.com>
2025-11-13 14:28:48 +0100

    added docker compose for windows and mac

--- setup.sh
+++ setup.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 echo "Starting Mario Kart RL Hackathon..."
 
-# Allow Docker to display windows
-xhost +
+# Detect platform and set appropriate display
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux detected - using native X11"
+    xhost +
+    export DISPLAY=:0
+    DOCKER_COMPOSE_FILE="docker-compose.linux.yml"
+    
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "macOS detected - using XQuartz"
+    export DISPLAY=host.docker.internal:0
+    DOCKER_COMPOSE_FILE="docker-compose.mac.yml"
+    
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    echo "Windows detected - using VcXsrv" 
+    export DISPLAY=host.docker.internal:0
+    DOCKER_COMPOSE_FILE="docker-compose.windows.yml"
+    
+else
+    echo "Unsupported platform: $OSTYPE"
+    exit 1
+fi
 
 echo "Building Python server..."
-docker-compose build python-server
+docker-compose -f $DOCKER_COMPOSE_FILE build python-server
 
 echo "Starting everything..."
-docker-compose up
+docker-compose -f $DOCKER_COMPOSE_FILE up
 
 #chmod +x setup.sh
 #./setup.sh
